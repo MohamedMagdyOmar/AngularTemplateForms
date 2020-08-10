9526e3e008709b8e4452f34b85c5f5e46bea59a2
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-08-10 15:00:46 -0400

    Copying From Data

--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -8,13 +8,18 @@ import { UserSettings } from '../data/user-settings';
 })
 export class UserSettingsFormComponent implements OnInit {
 
-  userSettings: UserSettings = {
+  originalUserSettings: UserSettings = {
     name: "Mohamed",
     emailOffers: true,
     interfaceStyle: 'dark',
     subscriptionType: 'Annual',
     notes: 'sadfasdffsa'
   };
+
+  // here we are using "spread operator ..." will take the original user settings, and copy each property
+  // so when we are changing values in the form, only the "userSettings" will be affected, but note that we make this copy
+  // incase the user click on back or did not submit the changed values, we still keep the default value and not losing them
+  userSettings: UserSettings = {... this.originalUserSettings}
 
   constructor() { }
 
