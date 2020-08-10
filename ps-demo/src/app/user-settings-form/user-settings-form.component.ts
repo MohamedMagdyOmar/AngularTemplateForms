e2749706f58e22ee911430703b20d75fdef0eb48
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-08-10 18:07:00 -0400

    Submitting Forms

--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -24,6 +25,10 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form.valid);
   }
 
 }
